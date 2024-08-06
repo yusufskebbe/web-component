21f547c37306df0b716756e78c86d431d3bb992b
yusuf <kebbeyusuf@gmail.com>
2024-08-06 11:46:56 +0300

    package implemetation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chromatic-com/storybook'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/web-components'],
   framework: {
     name: '@storybook/web-components-vite',
     options: {},
