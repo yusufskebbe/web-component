30289be281196ea591678646cdafe800aedd8463
yusuf <kebbeyusuf@gmail.com>
2024-08-06 12:32:20 +0300

    story modification

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -1,36 +1,65 @@
-export default {
+//
+
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+
+import { Components } from '../../components';
+
+const meta: Meta = {
   title: 'Components/Button',
-
-  tags: ['autodocs'],
   parameters: {
-    actions: {
-      argTypesRegex: '^on.*',
-      handles: ['click'],
-    },
+    layout: 'fullscreen',
   },
+  render: (args: Components.MyButton) =>
+    html`<my-button button-text="${args.buttonText}" button-type="${args.buttonType}" button-style="${args.buttonStyle}" button-disabled="${args.buttonDisabled}"></my-button>`,
+};
 
-  argTypes: {
-    buttonType: {
-      control: { type: 'radio' },
-      options: ['submit', 'button'],
-    },
-    buttonStyle: {
-      control: { type: 'select' },
-      options: ['primary', 'outlined', 'danger', 'success'],
-    },
-    buttonDisabled: {
-      control: { type: 'boolean' },
-    },
+export default meta;
+
+type Story = StoryObj<Components.MyButton>;
+
+export const MyButton: Story = {
+  args: {
+    buttonText: 'Anıl Buton',
+    buttonType: 'submit',
+    buttonStyle: 'danger',
+    buttonDisabled: false,
   },
 };
 
-const Template = ({ buttonText, buttonType, buttonStyle, buttonDisabled }) => `
-  <my-button button-text="${buttonText}" button-type="${buttonType}" button-style="${buttonStyle}" button-disabled="${buttonDisabled}"></my-button>`;
+//export default {
+//   title: 'Components/Button',
 
-export const Default = Template.bind({});
-Default.args = {
-  buttonText: 'Default',
-  buttonType: 'submit',
-  buttonStyle: 'primary',
-  buttonDisabled: 'false',
-};
+//   tags: ['autodocs'],
+//   parameters: {
+//     actions: {
+//       argTypesRegex: '^on.*',
+//       handles: ['click'],
+//     },
+//   },
+
+//   argTypes: {
+//     buttonType: {
+//       control: { type: 'radio' },
+//       options: ['submit', 'button'],
+//     },
+//     buttonStyle: {
+//       control: { type: 'select' },
+//       options: ['primary', 'outlined', 'danger', 'success'],
+//     },
+//     buttonDisabled: {
+//       control: { type: 'boolean' },
+//     },
+//   },
+// };
+
+// const Template = ({ buttonText, buttonType, buttonStyle, buttonDisabled }) => `
+//   <my-button button-text="${buttonText}" button-type="${buttonType}" button-style="${buttonStyle}" button-disabled="${buttonDisabled}"></my-button>`;
+
+// export const Default = Template.bind({});
+// Default.args = {
+//   buttonText: 'Default',
+//   buttonType: 'submit',
+//   buttonStyle: 'primary',
+//   buttonDisabled: 'false',
+// };
