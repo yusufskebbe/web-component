e5c5c628503409c3298f96361ca6f003283a216d
yusuf <kebbeyusuf@gmail.com>
2024-08-06 12:15:50 +0300

    text implemented

--- src/components/h2/Text.stories.ts
+++ src/components/h2/Text.stories.ts
@@ -12,6 +12,6 @@ export default {
 
 export const Template = () => `
   <my-text>
- 
+    text is here
   </my-text>
 `;
--- src/components/h2/Text.tsx
+++ src/components/h2/Text.tsx
@@ -2,11 +2,16 @@ import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'my-text',
+  styleUrl: 'Text.css',
 
   shadow: true,
 })
 export class Button {
   render() {
-    return <h2>Test Text</h2>;
+    return (
+      <h2>
+        <slot></slot>
+      </h2>
+    );
   }
 }
